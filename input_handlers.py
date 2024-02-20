af4031eb59fa197633296f5f95f03342923278d2
Grapes42 <f.inggrapes@gmail.com>
2024-02-21 11:09:13 +1300

    improved player input

--- input_handlers.py
+++ input_handlers.py
@@ -14,16 +14,51 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         key = event.sym
 
-        if key == tcod.event.K_UP:
+        """
+        Arrow key movement:
+               up
+               |
+        left — z — right
+               |
+              down
+
+        """
+        if key == tcod.event.KeySym.UP:
             action = MovementAction(dx=0, dy=-1)
-        elif key == tcod.event.K_DOWN:
+        elif key == tcod.event.KeySym.DOWN:
             action = MovementAction(dx=0, dy=1)
-        elif key == tcod.event.K_LEFT:
+        elif key == tcod.event.KeySym.LEFT:
             action = MovementAction(dx=-1, dy=0)
-        elif key == tcod.event.K_RIGHT:
+        elif key == tcod.event.KeySym.RIGHT:
             action = MovementAction(dx=1, dy=0)
 
-        elif key == tcod.event.K_ESCAPE:
+            """
+        YUHJKLBN Movement:
+         y  k  u
+          \ | /
+        h — z — l
+          / | \
+         b  j  n
+            """
+        elif key == tcod.event.KeySym.y:
+            action = MovementAction(dx=-1, dy=-1)
+        elif key == tcod.event.KeySym.k:
+            action = MovementAction(dx=0, dy=-1)
+        elif key == tcod.event.KeySym.u:
+            action = MovementAction(dx=+1, dy=-1)
+        elif key == tcod.event.KeySym.h:
+            action = MovementAction(dx=-1, dy=0)
+        elif key == tcod.event.KeySym.l:
+            action = MovementAction(dx=+1, dy=0)
+        elif key == tcod.event.KeySym.b:
+            action = MovementAction(dx=-1, dy=+1)
+        elif key == tcod.event.KeySym.j:
+            action = MovementAction(dx=0, dy=+1)
+        elif key == tcod.event.KeySym.n:
+            action = MovementAction(dx=+1, dy=+1)
+
+        # Quits program when esc is pressed 
+        elif key == tcod.event.KeySym.ESCAPE:
             action = EscapeAction()
 
         # No valid key was pressed
