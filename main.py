e46c5544ee6194c1aa98e8104f9c99d532f992a9
Grapes42 <f.inggrapes@gmail.com>
2024-02-22 12:58:18 +1300

    made simple test for room generation

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
+from procgen import generate_dungeon
 
 def main() -> None:
-    screen_width = 30
-    screen_height = 30
 
-    map_width = 30
-    map_height = 30
+    width = 80
+    height = 50
+
+    screen_width = width
+    screen_height = height
+
+    map_width = width
+    map_height = height
 
     tileset = tcod.tileset.load_tilesheet(
         "spritesheet.png", 32, 8, tcod.tileset.CHARMAP_TCOD
@@ -21,12 +25,9 @@ def main() -> None:
 
     player = Entity(int(screen_width / 2), int(screen_height / 2), "@")
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "f")
-    toeLicker = Entity(int(screen_width / 2 + 5), int(screen_height / 2), "T")
-    potion = Entity(int(screen_width / 2 - 5), int(screen_height / 2 + 5), "!")
-    potion = Entity(int(screen_width / 2 - 5), int(screen_height / 2 + 5), "!")
-    entities = {npc, toeLicker, potion, player}
+    entities = {npc, player}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(map_width, map_height)
 
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
